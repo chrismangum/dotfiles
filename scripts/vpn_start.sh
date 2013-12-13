8d2a78bd8bb6574b3ac91c58f6467b9b97c2e8e6
Chris Mangum <mtdbass5@gmail.com>
2013-12-13 10:53:13 -0500

    renamed a variable and moved logic that gets network address into a variable

--- scripts/vpn_start.sh
+++ scripts/vpn_start.sh
@@ -4,8 +4,9 @@ sudo iptables -F
 sudo /etc/init.d/openvpn stop
 
 pia_ip=$(host us-east.privateinternetaccess.com | head -n 1 | cut -d " " -f 4)
-def_int=$(ip link | grep "state UP" | sed -r "s/[0-9]: (.*): .*/\1/")
-my_ip=$(ifconfig $def_int | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1)
+iface=$(ip link | grep "state UP" | sed -r "s/[0-9]: (.*): .*/\1/")
+my_ip=$(ifconfig $iface | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1)
+net_addr=$(ip route | grep $my_ip | cut -d " " -f 1)
 
 echo "up /etc/openvpn/update-resolv-conf
 down /etc/openvpn/update-resolv-conf
@@ -31,6 +32,6 @@ sudo /etc/init.d/openvpn start
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A OUTPUT -o tun0 -j ACCEPT
 #allow traffic on local network
-sudo iptables -A OUTPUT -o $def_int -d $(ip route | grep $my_ip | cut -d " " -f 1) -j ACCEPT
-sudo iptables -A OUTPUT -o $def_int -d $pia_ip -j ACCEPT
+sudo iptables -A OUTPUT -o $iface -d $net_addr -j ACCEPT
+sudo iptables -A OUTPUT -o $iface -d $pia_ip -j ACCEPT
 sudo iptables -A OUTPUT -j DROP
