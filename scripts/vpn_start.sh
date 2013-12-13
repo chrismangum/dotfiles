373dd2d735c2f9745e18ef4b5055af0fb0448161
Chris Mangum <mtdbass5@gmail.com>
2013-12-13 10:42:36 -0500

    split out local ip getting into a variable for readability

--- scripts/vpn_start.sh
+++ scripts/vpn_start.sh
@@ -5,6 +5,7 @@ sudo /etc/init.d/openvpn stop
 
 pia_ip=$(host us-east.privateinternetaccess.com | head -n 1 | cut -d " " -f 4)
 def_int=$(ip link | grep "state UP" | sed -r "s/[0-9]: (.*): .*/\1/g")
+my_ip=$(ifconfig $def_int | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1)
 
 echo "up /etc/openvpn/update-resolv-conf
 down /etc/openvpn/update-resolv-conf
@@ -30,6 +31,6 @@ sudo /etc/init.d/openvpn start
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A OUTPUT -o tun0 -j ACCEPT
 #allow traffic on local network
-sudo iptables -A OUTPUT -o $def_int -d $(ip route | grep $(ifconfig $def_int | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1) | cut -d " " -f 1) -j ACCEPT
+sudo iptables -A OUTPUT -o $def_int -d $(ip route | grep $my_ip | cut -d " " -f 1) -j ACCEPT
 sudo iptables -A OUTPUT -o $def_int -d $pia_ip -j ACCEPT
 sudo iptables -A OUTPUT -j DROP
