7d678c335d2afc1733352665bc618ce98b7fc864
Chris Mangum <mtdbass5@gmail.com>
2014-01-14 12:35:18 -0500

    refactored vpn start script utilizing caolan's async node library

--- scripts/vpn_start.js
+++ scripts/vpn_start.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 var os = require('os'),
+  async = require('async'),
   fs = require('fs'),
   dns = require('dns'),
   exec = require('child_process').exec;
 
-var iface, pia_ip, net_addr,
+var iface, pia_ip,
   ifaces = os.networkInterfaces();
 
 function getIface() {
@@ -16,26 +17,9 @@ function getIface() {
   }
 }
 
-function run(command, callback) {
-  exec(command, function (err, stdout, stderr) {
-    if (callback) {
-      if (err) throw err;
-      callback(stdout);
-    } else {
-      process.stdout.write(stdout + stderr);
-    }
-  });
-}
-
-function writeFile(filename, string, callback) {
-  fs.writeFile(filename, string, function (err) {
-    if (err) throw err;
-    callback();
-  });
-}
-
-function writeVpnConfig(callback) {
-  writeFile('/etc/openvpn/client.conf', [
+function writeVpnConfig(addresses, callback) {
+  pia_ip = addresses[0];
+  fs.writeFile('/etc/openvpn/client.conf', [
     'up /etc/openvpn/update-resolv-conf',
     'down /etc/openvpn/update-resolv-conf',
     'script-security 2',
@@ -57,8 +41,9 @@ function writeVpnConfig(callback) {
   ].join(os.EOL), callback);
 }
 
-function writeIpRules(callback) {
-  writeFile('/etc/iptables.up.rules', [
+function writeIpRules(net_addr, stderr, callback) {
+  net_addr = net_addr.toString().split(' ')[0];
+  fs.writeFile('/etc/iptables.up.rules', [
     '*filter',
     ':INPUT ACCEPT [0:0]',
     ':FORWARD ACCEPT [0:0]',
@@ -72,35 +57,29 @@ function writeIpRules(callback) {
   ].join(os.EOL), callback);
 }
 
-function getNetAddress(callback) {
-  run('ip route | grep ' + ifaces[iface][0].address, function (output) {
-    net_addr = output.toString().split(' ')[0];
-    callback();
-  });
-}
-
-function getVpnIP(callback) {
-  dns.resolve4('us-east.privateinternetaccess.com', function (err, addresses) {
-    if (err) throw err;
-    pia_ip = addresses[0];
-    callback();
-  });
-}
-
 if (process.getuid() !== 0) {
-  run('sudo ./vpn_start.js');
+  console.log('run as sudo');
 } else {
   iface = getIface();
-  getVpnIP(function () {
-    writeVpnConfig(function () {
-      run('/etc/init.d/openvpn restart');
-    });
-    getNetAddress(function () {
-      writeIpRules(function () {
-        run('iptables-restore < /etc/iptables.up.rules');
-      });
-    });
-  });
+  async.waterfall([
+    function (callback) {
+      dns.resolve4('us-east.privateinternetaccess.com', callback);
+    },
+    writeVpnConfig,
+    function (callback) {
+      exec('/etc/init.d/openvpn restart', callback);
+    },
+    //get network address:
+    function (stdout, stderr, callback) {
+      exec('ip route | grep ' + ifaces[iface][0].address, callback);
+    },
+    writeIpRules,
+    function (callback) {
+      exec('iptables-restore < /etc/iptables.up.rules', callback);
+    }
+  ], function (err, result) {
+      if (err) throw err;
+      console.log('Done.');
+    }
+  );
 }
-
-
