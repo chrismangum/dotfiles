ff7c532304dfe42c7144e3e4e97612622f92545d
Chris Mangum <mtdbass5@gmail.com>
2014-01-16 10:46:33 -0500

    code refactoring

--- scripts/vpn_start.js
+++ scripts/vpn_start.js
@@ -9,16 +9,44 @@ var os = require('os'),
 
 var pia_ip;
 
-function getIP() {
-  var ifaces = os.networkInterfaces(),
-    iface = arrStrFind(_.keys(ifaces), 'eth');
-  return ifaces[iface][0].address;
+function log(msg) {
+  process.stdout.write(msg);
 }
 
 function arrStrFind(arr, string) {
   return _.find(arr, function (i) {
     return i.indexOf(string) !== -1;
   });
+}
+
+function grepOne(needle, haystack) {
+  return arrStrFind(haystack.split('\n'), needle);
+}
+
+function getIP() {
+  var ifaces = os.networkInterfaces(),
+    iface = arrStrFind(_.keys(ifaces), 'eth');
+  return ifaces[iface][0].address;
+}
+
+function getNetAddr(ipRoute) {
+  return grepOne(getIP(), ipRoute).split(' ')[0];
+}
+
+function writeIpRules(stdout, stderr, callback) {
+  log('Done.\nWriting iptables rule file: ');
+  fs.writeFile('/etc/iptables.up.rules', [
+    '*filter',
+    ':INPUT ACCEPT [0:0]',
+    ':FORWARD ACCEPT [0:0]',
+    ':OUTPUT ACCEPT [0:0]',
+    '-A OUTPUT -o lo -j ACCEPT',
+    '-A OUTPUT -o tun0 -j ACCEPT',
+    '-A OUTPUT -d ' + getNetAddr(stdout.toString()) + ' -j ACCEPT',
+    '-A OUTPUT -d ' + pia_ip + ' -j ACCEPT',
+    '-A OUTPUT -j DROP',
+    'COMMIT' + os.EOL
+  ].join(os.EOL), callback);
 }
 
 function writeVpnConfig(addresses, callback) {
@@ -46,30 +74,6 @@ function writeVpnConfig(addresses, callback) {
   ].join(os.EOL), callback);
 }
 
-function writeIpRules(net_addr, callback) {
-  log('Done.\nWriting iptables rule file: ');
-  fs.writeFile('/etc/iptables.up.rules', [
-    '*filter',
-    ':INPUT ACCEPT [0:0]',
-    ':FORWARD ACCEPT [0:0]',
-    ':OUTPUT ACCEPT [0:0]',
-    '-A OUTPUT -o lo -j ACCEPT',
-    '-A OUTPUT -o tun0 -j ACCEPT',
-    '-A OUTPUT -d ' + net_addr + ' -j ACCEPT',
-    '-A OUTPUT -d ' + pia_ip + ' -j ACCEPT',
-    '-A OUTPUT -j DROP',
-    'COMMIT' + os.EOL
-  ].join(os.EOL), callback);
-}
-
-function log(msg) {
-  process.stdout.write(msg);
-}
-
-function grepOne(needle, haystack) {
-  return arrStrFind(haystack.split('\n'), needle);
-}
-
 if (process.getuid() !== 0) {
   exec('sudo ./vpn_start.js', function (err, stdout, stderr) {
     log(stdout + stderr);
@@ -88,11 +92,6 @@ if (process.getuid() !== 0) {
     //get routes:
     function (stdout, stderr, callback) {
       exec('ip route', callback);
-    },
-    //isolate net addr:
-    function (stdout, stderr, callback) {
-      var net_addr = grepOne(getIP(), stdout.toString()).split(' ')[0];
-      callback(null, net_addr);
     },
     writeIpRules,
     function (callback) {
