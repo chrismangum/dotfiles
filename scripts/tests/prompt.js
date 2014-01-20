cbdb5091190ae6d8b299e466c7b246afcfbd6181
Chris Mangum <mtdbass5@gmail.com>
2014-01-20 11:06:34 -0500

    merged album query script into prompt and changed prompt method to readline

--- scripts/tests/prompt.js
+++ scripts/tests/prompt.js
@@ -1,19 +1,42 @@
 #!/usr/bin/env node
 
-var callback;
-process.stdin.on('data', function (res) {
-  process.stdin.pause();
-  callback(res);
-});
+var request = require('request'),
+  _ = require('underscore'),
+  util = require('util'),
+  readline = require('readline');
 
-function prompt(prompt, cback) {
-  callback = cback;
-  process.stdout.write(prompt);
-  process.stdin.resume();
+var items,
+  i = 0,
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+function prompt(item, callback) {
+  console.log('Album: ' + item.name);
+  console.log('Artist: ' + item.artist);
+  rl.question('Is this correct? (y, n): ', function (res) {
+    if (/y/i.test(res)) {
+      rl.close();
+      callback(items[i]);
+    } else if (items[i += 1]) {
+      prompt(items[i], callback);
+    } else {
+      rl.close();
+      console.log('No more items.');
+    }
+  });
 }
 
-prompt('Is this correct? (y, n): ', function (res) {
-  console.log(/y\n/i.test(res) ? 'Ok' : 'Aborting');
+request('http://ws.spotify.com/search/1/album.json?q=OK Computer', function (error, response, body) {
+  body = JSON.parse(body).albums.slice(0, 10);
+  body = _.map(body, function (i) {
+    i.id = i.href.split(':')[2]
+    i.artist = i.artists[0].name;
+    return _.pick(i, 'name', 'id', 'artist');
+  });
+  items = body;
+  prompt(items[i], function (sel) {
+    console.log('The selected item was: ' + util.inspect(sel));
+  });
 });
-
-
