9370843a04aac09029d7bb97be5ede3723c81524
Chris Mangum <mtdbass5@gmail.com>
2014-02-13 17:21:57 -0500

    corrected name of arch rc.lua and added dropbox startup command

--- awesome/rc_arch.lua
+++ awesome/rc_arch.lua
@@ -435,3 +435,15 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Startup:
+
+do
+  local cmds =
+  {
+    "dropbox start"
+  }
+  for _,i in pairs(cmds) do
+    awful.util.spawn(i)
+  end
+end
