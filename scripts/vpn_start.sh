a294a80192bb0b9a9ed294e06cf36144eca77ec4
Chris Mangum <mtdbass5@gmail.com>
2014-09-16 22:45:21 -0400

    Vpn script: updated some variable names

--- scripts/vpn_start.sh
+++ scripts/vpn_start.sh
@@ -2,8 +2,8 @@
 sudo iptables -F
 sudo systemctl stop openvpn@client.service
 
-pia_ip=$(host us-east.privateinternetaccess.com | head -n 1 | cut -d " " -f 4)
-net_addr=$(ip route show scope link | grep -E "dev e(n|th)" | cut -d " " -f 1)
+PIA=$(host us-east.privateinternetaccess.com | head -n 1 | cut -d " " -f 4)
+CIDR=$(ip route show scope link | grep -E "dev e(n|th)" | cut -d " " -f 1)
 
 echo "up /etc/openvpn/update-resolv-conf
 down /etc/openvpn/update-resolv-conf
@@ -11,7 +11,7 @@ script-security 2
 client
 dev tun
 proto udp
-remote $pia_ip 1194
+remote $PIA 1194
 resolv-retry infinite
 nobind
 persist-key
@@ -29,8 +29,8 @@ sudo systemctl start openvpn@client.service
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A OUTPUT -o tun0 -j ACCEPT
 #allow traffic on local network
-sudo iptables -A OUTPUT -d $net_addr -j ACCEPT
-sudo iptables -A OUTPUT -d $pia_ip -j ACCEPT
+sudo iptables -A OUTPUT -d $CIDR -j ACCEPT
+sudo iptables -A OUTPUT -d $PIA -j ACCEPT
 sudo iptables -A OUTPUT -j DROP
 
-echo "Connected to: $pia_ip"
+echo "Connected to: $PIA"
