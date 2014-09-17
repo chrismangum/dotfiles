f24278d3453e731cbd6d0365d00269fa729244d3
Chris Mangum <mtdbass5@gmail.com>
2014-09-16 21:50:47 -0400

    Deprecated node script in favor of smaller bash script

--- scripts/vpn_start.sh
+++ scripts/vpn_start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 sudo iptables -F
-sudo /etc/init.d/openvpn stop
+sudo systemctl stop openvpn@client.service
 
 pia_ip=$(host us-east.privateinternetaccess.com | head -n 1 | cut -d " " -f 4)
-iface=$(ip link | grep "state UP" | sed -r "s/[0-9]: (.*): .*/\1/")
-my_ip=$(ifconfig $iface | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" | head -n 1)
-net_addr=$(ip route | grep $my_ip | cut -d " " -f 1)
+net_addr=$(ip route show scope link | grep -E "dev e(n|th)" | cut -d " " -f 1)
 
 echo "up /etc/openvpn/update-resolv-conf
 down /etc/openvpn/update-resolv-conf
@@ -27,11 +24,11 @@ comp-lzo
 verb 1
 reneg-sec 0" | sudo tee /etc/openvpn/client.conf
 
-sudo /etc/init.d/openvpn start
+sudo systemctl start openvpn@client.service
 
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -A OUTPUT -o tun0 -j ACCEPT
 #allow traffic on local network
-sudo iptables -A OUTPUT -o $iface -d $net_addr -j ACCEPT
-sudo iptables -A OUTPUT -o $iface -d $pia_ip -j ACCEPT
+sudo iptables -A OUTPUT -d $net_addr -j ACCEPT
+sudo iptables -A OUTPUT -d $pia_ip -j ACCEPT
 sudo iptables -A OUTPUT -j DROP
