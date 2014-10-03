bcc506ffab3518aff84e9017867f399562ab6a38
Chris Mangum <mtdbass5@gmail.com>
2014-10-03 16:48:14 -0400

    Added a call to vbox xrandr script in i3 config

--- scripts/set_vbox_res.sh
+++ scripts/set_vbox_res.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-modeLine=$(cvt 1280 800 | grep Modeline)
-modeName=$(echo $modeLine | cut -d ' ' -f 2)
-modeValue=$(echo $modeLine | sed 's/Modeline //')
+if [[ $(hostname) == 'archtop' ]]; then
+  modeLine=$(cvt 1280 800 | grep Modeline)
+  modeName=$(echo $modeLine | cut -d ' ' -f 2)
+  modeValue=$(echo $modeLine | sed 's/Modeline //')
 
-xrandr --newmode $modeValue
-xrandr --addmode VBOX1 $modeName
-xrandr --output VBOX1 --mode $modeName --left-of VBOX0 --output VBOX0 --mode 1920x1080
+  xrandr --newmode $modeValue
+  xrandr --addmode VBOX1 $modeName
+  xrandr --output VBOX1 --mode $modeName --left-of VBOX0 --output VBOX0 --mode 1920x1080
+fi
