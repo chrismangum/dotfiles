a58b5e9461aafdd2cbd982b60685204ea5c1b040
Chris Mangum <mtdbass5@gmail.com>
2014-10-26 15:15:37 -0400

    Switched to using stow for isntalling dotfiles, added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+#create symlinks
+stow -v alsa fonts git i3 mutt rtorrent tmux urlview vim xorg yaourt zsh
+
+#install fonts
+fc-cache -vf $HOME/.fonts
+
+#install vim plugins
+if [[ ! -d ~/.vim/bundle ]]; then
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+fi
