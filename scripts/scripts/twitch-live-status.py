fbb031a3170899054f50016874c5eec39cd519f7
Chris Mangum <mtdbass5@gmail.com>
2015-09-15 00:18:50 -0400

    Added twitch live status script

---
+++ scripts/scripts/twitch-live-status.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import json
+import sys
+from urllib import request
+
+HEADERS = {
+    'Accept:': 'application/vnd.twitchtv.v3+json'
+}
+BASEURL = 'https://api.twitch.tv/kraken/'
+
+def getFollowing(username):
+    data = getJSON(BASEURL + 'users/' + sys.argv[1] + '/follows/channels/?limit=100', HEADERS)
+    return sorted([ch['channel']['name'] for ch in data['follows']])
+
+def getJSON(url, headers):
+    return json.loads(httpGet(url, headers))
+
+def getLiveChannels(channels):
+    data = getJSON(BASEURL + 'streams/?channel=' + ','.join(channels), HEADERS)
+    ret = {}
+    for ch in data['streams']:
+        ret[ch['channel']['name']] = {
+            'game': ch['channel']['game'],
+            'title': ch['channel']['status'],
+            'viewers': ch['viewers']
+        }
+    return ret
+
+def httpGet(url, headers):
+    req = request.Request(url, headers=headers)
+    return request.urlopen(req).readall().decode('utf-8')
+
+def prettyPrint(data):
+    return json.dumps(data, indent=2)
+
+if (len(sys.argv) > 1):
+    liveChannels = getLiveChannels(getFollowing(sys.argv[1]))
+    out = 'Found ' + str(len(liveChannels)) + ' live channels'
+    if (len(liveChannels)):
+        out += ': ' + prettyPrint(liveChannels)
+    print(out)
+else:
+    print('Please supply a twitch username.')
+    sys.exit(1)
