04b55d2a78ca83e0991906570d3b633603dafeec
Chris Mangum <mtdbass5@gmail.com>
2015-12-03 14:10:30 -0500

    VPN firewall: added reset at start of script and suppressed output to reset and rule-adding portions

--- scripts/scripts/vpn_firewall.sh
+++ scripts/scripts/vpn_firewall.sh
@@ -5,25 +5,29 @@ main_interface=$(ip route show scope link | grep -E 'dev e(n|th)' | awk '{print 
 local_network=$(ip route show scope link | grep -E "$main_interface" | awk '{print $1}')
 pia=$(ip route show scope link | grep 'dev tun0' | awk '{print $NF}')
 
-# Default policies
-sudo ufw default deny incoming
-sudo ufw default deny outgoing
+{
+    sudo ufw --force reset
 
-# Openvpn interface
-sudo ufw allow in on tun0
-sudo ufw allow out on tun0
+    # Default policies
+    sudo ufw default deny incoming
+    sudo ufw default deny outgoing
 
-# Local Network
-sudo ufw allow in on $main_interface from $local_network
-sudo ufw allow out on $main_interface to $local_network
+    # Openvpn interface
+    sudo ufw allow in on tun0
+    sudo ufw allow out on tun0
 
-# Openvpn
-sudo ufw allow out on $main_interface to $pia
-sudo ufw allow in on $main_interface from $pia
+    # Local Network
+    sudo ufw allow in on $main_interface from $local_network
+    sudo ufw allow out on $main_interface to $local_network
 
-# DNS
-sudo ufw allow in from any to any port 53
-sudo ufw allow out from any to any port 53
+    # Openvpn
+    sudo ufw allow out on $main_interface to $pia
+    sudo ufw allow in on $main_interface from $pia
+
+    # DNS
+    sudo ufw allow in from any to any port 53
+    sudo ufw allow out from any to any port 53
+} &> /dev/null
 
 sudo ufw enable
 sudo ufw status
