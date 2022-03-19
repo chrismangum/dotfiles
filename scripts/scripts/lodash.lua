7e19df5a6c9a7cd30cb96f384a0915272c01045f
Chris Mangum <cmagnum@cisco.com>
2022-03-19 18:34:23 -0400

    switching to method syntax, organization

--- scripts/scripts/lodash.lua
+++ scripts/scripts/lodash.lua
@@ -1,152 +1,299 @@
-local _ = require('fun')
+local fun = require('fun')
+local _ = {}
 
-local exports = {}
+-- Array
+function _.concat (...)
+	return fun.totable(fun.chain(table.unpack(_.map({...}, _.castArray))))
+end
 
+function _.drop (array, n)
+	if _.isArray(array) then
+		return fun.totable(fun.drop(n or 1, array))
+	end
+	return {}
+end
 
--- Helpers (called by other functions)
-local function nthArg (index)
-	return function (...)
-		local args = {...}
-		return args[index]
+function _.fromPairs (_pairs)
+	return _.reduce(_pairs, function (result, pair)
+		result[pair[1]] = pair[2]
+		return result
+	end, {})
+end
+
+function _.head (array)
+	if _.isArray(array) then
+		return array[1]
 	end
 end
-exports.nthArg = nthArg
 
-local size = _.length
-exports.size = size
+function _.indexOf (collection, value)
+	if _.isArray(collection) then
+		return fun.index_of(value, collection)
+	elseif _.isString(collection) then
+		local start, _ = string.find(collection, value)
+		return start
+	end
+end
 
-local function reverse (array)
+function _.intersection (...)
+	local arrays = {...}
+	local tailArrays = _.tail(arrays)
+	return _.reduce(_.uniq(_.head(arrays)), function (result, value)
+		if _.every(tailArrays, function (array) return _.includes(array, value) end) then
+			table.insert(result, value)
+		end
+		return result
+	end, {})
+end
+
+function _.join (array, separator)
+	return table.concat(array, separator or ',')
+end
+
+function _.last (array)
+	if _.isArray(array) then
+		return array[_.size(array)]
+	end
+end
+
+function _.reverse (array)
 	local reversed = {}
-	for i = size(array), 1, -1 do
+	for i = _.size(array), 1, -1 do
 		table.insert(reversed, array[i])
 	end
 	return reversed
 end
-exports.reverse = reverse
 
-local function flip (func)
-	return function (...)
-		return func(table.unpack(reverse({...})))
-	end
-end
-exports.flip = flip
-
-local add = _.operator.add
-exports.add = add
-
-local every = flip(_.every)
-exports.every = every
-
-local some = flip(_.some)
-exports.some = some
-
-local identity = nthArg(1)
-exports.identity = identity
-
-local function isNil (value)
-	return value == nil
-end
-exports.isNil = isNil
-
-local function isEmpty (value)
-	return isNil(value) or _.is_null(value)
-end
-exports.isEmpty = isEmpty
-
-local function isNumber (value)
-	return type(value) == 'number'
-end
-exports.isNumber = isNumber
-
-local function isObject (value)
-	return type(value) == 'table'
-end
-exports.isObject = isObject
-
-local function isString (value)
-	return type(value) == 'string'
-end
-exports.isString = isString
-
-local function isArray (value)
-	if not isObject(value) then
-		return false
-	elseif isEmpty(value) then
-		return true
-	end
-	local gen, param, state = _.iter(value)
-	return isNumber(state)
-end
-exports.isArray = isArray
-
-local function drop (array, n)
-	if isArray(array) then
-		return _.totable(_.drop(n or 1, array))
+function _.take (array, n)
+	if _.isArray(array) then
+		return fun.totable(fun.take(n or 1, array))
 	end
 	return {}
 end
-exports.drop = drop
 
-local head = function (array)
-	if isArray(array) then
-		return array[1]
+function _.takeRight (array, n)
+	return _.take(_.reverse(array), n)
+end
+
+function _.union (...)
+	return _.uniq(_.concat(...))
+end
+
+function _.uniq (array)
+	return _.reduce(array, function (result, value)
+		if not _.includes(result, value) then
+			table.insert(result, value)
+		end
+		return result
+	end, {})
+end
+
+function _.without (array, ...)
+	local exclude = {...}
+	return _.reduce(array, function (result, value)
+		if not _.includes(exclude, value) then
+			table.insert(result, value)
+		end
+		return result
+	end, {})
+end
+
+function _.zip (...)
+	local arrays = {...}
+	return _.times(_.size(_.head(arrays)), function (index)
+		return _.map(arrays, _.property(index))
+	end)
+end
+
+function _.zipObject (keys, values)
+	return _.fromPairs(_.zip(keys, values))
+end
+
+-- Collection
+function _.every (collection, iteratee)
+	return fun.every(iteratee or _.identity, collection)
+end
+
+function _.forEach (collection, iteratee)
+	fun.foreach(iteratee or _.identity, collection)
+	return collection
+end
+
+function _.includes (collection, value)
+	if _.isPlainObject(collection) then
+		collection = _.values(collection)
+	end
+	return _.indexOf(collection, value) ~= nil
+end
+
+function _.map (collection, iteratee)
+	return fun.totable(fun.map(iteratee or _.identity, collection))
+end
+
+function _.partition (collection, iteratee)
+	local yes, no = fun.partition(iteratee or _.identity, collection)
+	return _.map({ yes, no }, fun.totable);
+end
+
+function _.reject (collection, predicate)
+	return _.filter(collection, _.negate(predicate))
+end
+
+function _.sample (collection, pop)
+	if _.isPlainObject(collection) then
+		collection = _.values(collection)
+	end
+	if _.isArray(collection) then
+		if (pop) then
+			return table.remove(collection, _.random(1, _.size(collection)))
+		end
+		return collection[_.random(1, _.size(collection))]
 	end
 end
-exports.head = head
 
-local function castArray (...)
+function _.sampleSize (collection, n)
+	if _.isPlainObject(collection) then
+		collection = _.values(collection)
+	else
+		collection = _.clone(collection)
+	end
+	local length = _.size(collection)
+	n = n or 1
+	if n > length then
+		n = length
+	end
+	return _.times(n, function () return _.sample(collection, true) end)
+end
+
+function _.sortBy (collection, iteratee)
+	if _.isPlainObject(collection) then
+		collection = _.values(collection)
+	else
+		collection = _.clone(collection)
+	end
+	if _.isString(iteratee) then
+		local key = iteratee
+		iteratee = _.overArgs(_.lt, {_.property(key), _.property(key)})
+	end
+	table.sort(collection, iteratee or _.lt)
+	return collection
+end
+
+-- Function
+function _.ary (func, n)
+	return _.rearg(func, _.range(n))
+end
+
+function _.flip (func)
+	return function (...)
+		return func(table.unpack(_.reverse({...})))
+	end
+end
+
+function _.negate (func)
+	return function (...)
+		return not func(...)
+	end
+end
+
+function _.overArgs (func, transforms)
+	transforms = _.castArray(transforms)
+	return function (...)
+		local args = {...}
+		return func(table.unpack(_.times(_.size(args), function (index)
+			if transforms[index] then
+				return transforms[index](args[index])
+			end
+			return args[index]
+		end)))
+	end
+end
+
+function _.rearg (func, indexes)
+	return function (...)
+		return func(table.unpack(_.map(indexes, _.propertyOf({...}))))
+	end
+end
+
+function _.unary (func)
+	return _.ary(func, 1)
+end
+
+-- Lang
+function _.castArray (...)
 	local args = {...}
 	local value = args[1]
-	if isEmpty(args) then
+	if _.isEmpty(args) then
 		return {}
-	elseif isArray(value) then
+	elseif _.isArray(value) then
 		return value
 	else
 		return {value}
 	end
 end
-exports.castArray = castArray
 
-local function isPlainObject (value)
-	return isObject(value) and not isArray(value)
-end
-exports.isPlainObject = isPlainObject
-
-local function indexOf (collection, value)
-	if isArray(collection) then
-		return _.index_of(value, collection)
-	elseif isString(collection) then
-		local start, _ = string.find(collection, value)
-		return start
+function _.clone (value)
+	if _.isArray(value) then
+		return _.map(value)
+	elseif _.isObject(value) then
+		return _.mapKeys(value)
 	end
+	return value
 end
-exports.indexOf = indexOf
 
-local function includes (collection, value)
-	if isPlainObject(collection) then
-		return indexOf(values(collection), value) ~= nil
+function _.isArray (value)
+	if not _.isObject(value) then
+		return false
+	elseif _.isEmpty(value) then
+		return true
 	end
-	return indexOf(collection, value) ~= nil
+	local gen, param, state = fun.iter(value)
+	return _.isNumber(state)
 end
-exports.includes = includes
 
-local function map (collection, iteratee)
-	return _.totable(_.map(iteratee or identity, collection))
+function _.isBoolean (value)
+	return type(value) == 'boolean'
 end
-exports.map = map
 
-local function negate (func)
-	return function (...)
-		return not func(...)
-	end
+function _.isEmpty (value)
+	return _.isNil(value) or fun.is_null(value)
 end
-exports.negate = negate
 
-local function random (...)
+function _.isFunction (value)
+	return type(value) == 'function'
+end
+
+function _.isNil (value)
+	return value == nil
+end
+
+function _.isNumber (value)
+	return type(value) == 'number'
+end
+
+function _.isObject (value)
+	return type(value) == 'table'
+end
+
+function _.isPlainObject (value)
+	return _.isObject(value) and not _.isArray(value)
+end
+
+function _.isString (value)
+	return type(value) == 'string'
+end
+
+-- Math
+function _.sum (array)
+	return _.reduce(array, _.add, 0)
+end
+
+-- Number
+function _.random (...)
 	local args = {...}
 	local lower, upper
 	local float = args[3] or false
-	if size(args) == 1 then
+	if _.size(args) == 1 then
 		lower = 0
 		upper = args[1]
 	else
@@ -158,360 +305,115 @@ local function random (...)
 	end
 	return math.random(lower, upper)
 end
-exports.random = random
-
-local function property (key)
-	return function (object)
-		return object[key]
-	end
-end
-exports.property = property
-
-local function propertyOf (object)
-	return function (key)
-		return object[key]
-	end
-end
-
-local function rearg (func, indexes)
-	return function (...)
-		return func(table.unpack(map(indexes, propertyOf({...}))))
-	end
-end
-
-local reduce = rearg(_.reduce, {2, 3, 1})
-exports.reduce = reduce
-
-local function flow (funcs)
-	return function (...)
-		return reduce(drop(funcs), function (result, func)
-			return func(result)
-		end, head(funcs)(...))
-	end
-end
-
-local range = flow({_.range, _.totable})
-exports.range = range
-
-local function times (n, iteratee)
-	return map(range(n), iteratee or identity)
-end
-exports.times = times
-
-local function ary (func, n)
-	return rearg(func, range(n))
-end
-exports.ary = ary
-
-local function unary (func)
-	return ary(func, 1)
-end
-exports.unary = unary
-
-local tail = unary(drop)
-exports.tail = tail
-
-local function values (object)
-	return map(object, nthArg(2))
-end
-exports.values = values
-
-local function fromPairs (_pairs)
-	return reduce(_pairs, function (result, pair)
-		result[pair[1]] = pair[2]
-		return result
-	end, {})
-end
-exports.fromPairs = fromPairs
-
-local function zip (...)
-	local arrays = {...}
-	return times(size(head(arrays)), function (index)
-		return map(arrays, property(index))
-	end)
-end
-exports.zip = zip
-
-local function zipObject (keys, values)
-	return fromPairs(zip(keys, values))
-end
-exports.zipObject = zipObject
-
-local function mapKeys (object, iteratee)
-	return zipObject(map(object, iteratee or identity), values(object))
-end
-exports.mapKeys = mapKeys
-
-local function mapValues (object, iteratee)
-	return zipObject(keys(object), map(object, iteratee or nthArg(2)))
-end
-exports.mapValues = mapValues
-
-local function clone (value)
-	if isArray(value) then
-		return map(value)
-	elseif isObject(value) then
-		return mapKeys(value)
-	end
-	return value
-end
-exports.clone = clone
-
-local overArgs = function (func, transforms)
-	transforms = castArray(transforms)
-	return function (...)
-		local args = {...}
-		return func(table.unpack(times(size(args), function (index)
-			if transforms[index] then
-				return transforms[index](args[index])
-			end
-			return args[index]
-		end)))
-	end
-end
-exports.overArgs = overArgs
-
-
--- Array
-local function concat (...)
-	return _.totable(_.chain(table.unpack(map({...}, castArray))))
-end
-exports.concat = concat
-
-local dropWhile = flow({flip(_.drop_while), _.totable})
-exports.dropWhile = dropWhile
-
-local function uniq (array)
-	return reduce(array, function (result, value)
-		if not includes(result, value) then
-			table.insert(result, value)
-		end
-		return result
-	end, {})
-end
-exports.uniq = uniq
-
-local function intersection (...)
-	local arrays = {...}
-	local tailArrays = tail(arrays)
-	return reduce(uniq(head(arrays)), function (result, value)
-		if every(tailArrays, function (array) return includes(array, value) end) then
-			table.insert(result, value)
-		end
-		return result
-	end, {})
-end
-exports.intersection = intersection
-
-local function join (array, separator)
-	return table.concat(array, separator or ',')
-end
-exports.join = join
-
-local function last (array)
-	if isArray(array) then
-		return array[size(array)]
-	end
-end
-exports.last = last
-
-local function take (array, n)
-	if isArray(array) then
-		return _.totable(_.take(n or 1, array))
-	end
-	return {}
-end
-exports.take = take
-
-local function takeRight (array, n)
-	return take(reverse(array), n)
-end
-exports.takeRight = takeRight
-
-local takeWhile = flow({flip(_.take_while), _.totable})
-exports.takeWhile = takeWhile
-
-local function union (...)
-	return uniq(concat(...))
-end
-exports.union = union
-
-local function without (array, ...)
-	local exclude = {...}
-	return reduce(array, function (result, value)
-		if not includes(exclude, value) then
-			table.insert(result, value)
-		end
-		return result
-	end, {})
-end
-exports.without = without
-
-
--- Collection
-local function every (collection, iteratee)
-	return _.every(iteratee or identity, collection)
-end
-exports.every = every
-
-local filter = flow({flip(_.filter), _.totable})
-exports.filter = filter
-
-local function reject (collection, predicate)
-	return filter(collection, negate(predicate))
-end
-exports.reject = reject
-
-local function forEach (collection, iteratee)
-	_.foreach(iteratee or identity, collection)
-	return collection
-end
-exports.forEach = forEach
-
-local function partition (collection, iteratee)
-	local yes, no = _.partition(iteratee or identity, collection)
-	return map({ yes, no }, _.totable);
-end
-exports.partition = partition
-
-local function sample (collection, pop)
-	if isPlainObject(collection) then
-		collection = values(collection)
-	end
-	if isArray(collection) then
-		if (pop) then
-			return table.remove(collection, random(1, size(collection)))
-		end
-		return collection[random(1, size(collection))]
-	end
-end
-exports.sample = sample
-
-local function sampleSize (collection, n)
-	if isPlainObject(collection) then
-		collection = values(collection)
-	else
-		collection = clone(collection)
-	end
-	local length = size(collection)
-	n = n or 1
-	if n > length then
-		n = length
-	end
-	return times(n, function () return sample(collection, true) end)
-end
-exports.sampleSize = sampleSize
-
-local function sortBy (collection, iteratee)
-	if isPlainObject(collection) then
-		collection = values(collection)
-	else
-		collection = clone(collection)
-	end
-	if isString(iteratee) then
-		local key = iteratee
-		iteratee = overArgs(_.operator.lt, {property(key), property(key)})
-	end
-	table.sort(collection, iteratee or _.operator.lt)
-	return collection
-end
-exports.sortBy = sortBy
-
-
--- Lang
-local function isBoolean (value)
-	return type(value) == 'boolean'
-end
-exports.isBoolean = isBoolean
-
-local function isFunction (value)
-	return type(value) == 'function'
-end
-exports.isFunction = isFunction
-
-local toString = tostring
-exports.toString = toString
-
 
 -- Object
-local function assign (...)
-	return _.tomap(_.chain(...))
+function _.assign (...)
+	return fun.tomap(fun.chain(...))
 end
-exports.assign = assign
 
-local function keys (object)
-	return map(object)
+function _.keys (object)
+	return _.map(object)
 end
-exports.keys = keys
 
-
--- Math
-local max = _.max
-exports.max = max
-
-local min = _.min
-exports.min = min
-
-
-local function sum (array)
-	return reduce(array, add, 0)
+function _.mapKeys (object, iteratee)
+	return _.zipObject(_.map(object, iteratee or _.identity), _.values(object))
 end
-exports.sum = sum
 
+function _.mapValues (object, iteratee)
+	return _.zipObject(_.keys(object), _.map(object, iteratee or _.nthArg(2)))
+end
+
+function _.values (object)
+	return _.map(object, _.nthArg(2))
+end
 
 -- String
-local function endsWith (str, target)
-	return target == '' or str:sub(-size(target)) == target
+function _.endsWith (str, target)
+	return target == '' or str:sub(-_.size(target)) == target
 end
-exports.endsWith = endsWith
 
-local function startsWith (str, target)
-	return str:sub(1, size(target)) == target
+function _.startsWith (str, target)
+	return str:sub(1, _.size(target)) == target
 end
-exports.startsWith = startsWith
-
-local toLower = string.lower
-exports.toLower = toLower
-
-local toUpper = string.upper
-exports.toUpper = toUpper
-
 
 -- Util
-local function constant (value)
+function _.constant (value)
 	return function () return value end
 end
-exports.constant = constant
 
-local function inspect (value, inner)
-	if isArray(value) then
-		return '[' .. join(map(value, function (val)
-			return inspect(val, true)
+function _.flow (funcs)
+	return function (...)
+		return _.reduce(_.drop(funcs), function (result, func)
+			return func(result)
+		end, _.head(funcs)(...))
+	end
+end
+
+function _.inspect (value, inner)
+	if _.isArray(value) then
+		return '[' .. _.join(_.map(value, function (val)
+			return _.inspect(val, true)
 		end), ', ') .. ']'
-	elseif isObject(value) then
+	elseif _.isObject(value) then
 		local str = '{'
 		local first = true
-		forEach(value, function (key, val)
+		_.forEach(value, function (key, val)
 			if first then
 				first = false
 			else
 				str = str .. ', '
 			end
-			str = str .. key .. ': ' .. inspect(val, true)
+			str = str .. key .. ': ' .. _.inspect(val, true)
 		end)
 		return str .. '}'
-	elseif inner and isString(value) then
+	elseif inner and _.isString(value) then
 		return '\'' .. value .. '\''
 	else
-		return toString(value)
+		return _.toString(value)
 	end
 end
-exports.inspect = inspect
 
-local function noop () end
-exports.noop = noop
+function _.noop () end
 
+function _.nthArg (index)
+	return function (...)
+		local args = {...}
+		return args[index]
+	end
+end
 
--- Testing Area
+function _.property (key)
+	return function (object)
+		return object[key]
+	end
+end
 
-return exports
+function _.propertyOf (object)
+	return function (key)
+		return object[key]
+	end
+end
+
+function _.times (n, iteratee)
+	return _.map(_.range(n), iteratee or _.identity)
+end
+
+_.add = fun.operator.add
+_.dropWhile = _.flow({_.flip(fun.drop_while), fun.totable})
+_.every = _.flip(fun.every)
+_.filter = _.flow({_.flip(fun.filter), fun.totable})
+_.identity = _.nthArg(1)
+_.lt = fun.operator.lt
+_.max = fun.max
+_.min = fun.min
+_.range = _.flow({fun.range, fun.totable})
+_.reduce = _.rearg(fun.reduce, {2, 3, 1})
+_.size = fun.length
+_.some = _.flip(fun.some)
+_.tail = _.unary(_.drop)
+_.takeWhile = _.flow({_.flip(fun.take_while), fun.totable})
+_.toLower = string.lower
+_.toString = tostring
+_.toUpper = string.upper
+
+return _
