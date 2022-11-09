3fdea5fd7f4c4b17b34ef33bdb9c35313de5945d
Chris Mangum <cmagnum@cisco.com>
2022-11-08 21:23:31 -0500

    removing stuff pili don't need

--- install.sh
+++ install.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-
 set -e
 
-mkdir -pv ~/.config/systemd/user \
-  ~/.local/share/xorg \
-
-#copy and add systemd user services
-if [[ -x systemctl ]]; then
-    cp -v systemd/.config/systemd/user/* ~/.config/systemd/user
-    for i in ~/.config/systemd/user/*.service; do systemctl --user enable $(basename $i); done;
-fi
-
 #create symlinks
-stow -v alsa bash cups fonts git gtk i3 mintty mongo mpv mutt redshift rtorrent scripts tmux \
-  urlview vim xorg
+stow -v bash fonts git mintty tmux vim
 
 #install fonts
 if [[ -x fc-cache ]]; then
--- scripts/scripts/lines.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-def echoLines(n):
-    for i in range(1, n + 1):
-        print('XxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXxXx', i)
-
-echoLines(int(sys.argv[1]))
