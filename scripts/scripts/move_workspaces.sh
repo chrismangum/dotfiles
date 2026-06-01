c7a9c449a347c87fc0308e1ffccb224a011f0f5b
Chris Mangum <cmagnum@cisco.com>
2026-06-01 09:41:55 -0400

    updates

--- scripts/scripts/move_workspaces.sh
+++ scripts/scripts/move_workspaces.sh
@@ -1,8 +1,13 @@
 DISPLAY=$(xrandr | grep -P '(?<!eDP-1) connected ' | awk '{print $1}')
+# swap below to move all to laptop screen:
 # DISPLAY=eDP-1
-for ws in $(i3-msg -t get_workspaces | jq -r '.[].name'); do
-    i3-msg workspace "$ws"
-    if [[ "$ws" != "7" ]]; then
-      i3-msg move workspace to output $DISPLAY
-    fi
+json=$(i3-msg -t get_workspaces | jq -c '.[]')
+nums=$(echo $json | jq 'select(.num != 7) | .num')
+focused_name=$(echo $json | jq 'select(.focused == true) | .name')
+for num in $nums; do
+  name=$(echo $json | jq "select(.num == $num) | .name")
+  i3-msg workspace $name
+  i3-msg move workspace to output $DISPLAY
 done
+# return focus:
+i3-msg workspace $focused_name
